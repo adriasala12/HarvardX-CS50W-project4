a1fb13a8bf4b90f2b838cd1904297064264818aa
Adrià Sala <adriasala124@gmail.com>
2020-07-20 14:24:50 +0200

    Fixed typo

--- network/models.py
+++ network/models.py
@@ -9,8 +9,4 @@ class Post(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
     content = models.TextField()
     datetime = models.DateTimeField()
-<<<<<<< HEAD
     likes = models.ManyToManyField(User)
-=======
-    likes = models.ManyToMany(User)
->>>>>>> 3f4c8cd4cb57164b3338ab91db57354547a01ee4
