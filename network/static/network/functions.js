e508327b57c0eceeea75eeffaa77e9186958ea93
Adrià Sala <adriasala124@gmail.com>
2020-07-21 13:17:10 +0200

    Like/unlike async functionality working

--- network/static/network/functions.js
+++ network/static/network/functions.js
@@ -12,3 +12,15 @@ function newPost() {
         newPostButton.style.display = "block";
     }
 }
+
+function like(id) {
+
+    fetch(`like/${id}`)
+    .then(response => response.json())
+    .then(result => {
+        console.log(result.result.html);
+        document.getElementById(`heart-${id}`).innerHTML = result.result.html
+        document.getElementById(`likes-${id}`).innerHTML = result.result.likes
+    });
+
+}
