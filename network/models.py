5deed0a178131986e749e9ac55945e23bd8eac81
Adrià Sala <adriasala124@gmail.com>
2020-07-22 12:16:15 +0200

    Changed followers/following in model User

--- network/models.py
+++ network/models.py
@@ -1,10 +1,8 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-
 class User(AbstractUser):
-    followers = models.ManyToManyField("self")
-    following = models.ManyToManyField("self")
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following')
 
 class Post(models.Model):
     username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
